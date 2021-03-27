564f745437b95b3394e063af603387721e0e0868
steve <ssosik@akamai.com>
2021-03-26 21:40:56 -0400

    WIP

--- week2/programming-assignment.py
+++ week2/programming-assignment.py
@@ -5,24 +5,42 @@ from pdb import set_trace
 # pycrypto: https://www.dlitz.net/software/pycrypto/api/current/
 # Hex to bytes: https://www.coursera.org/learn/crypto/discussions/weeks/2/threads/y3hgXmO7Eeuhwwo1pM_uuQ
 
-def chunks(lst, n):
+def chunker(lst, n):
     """Yield successive n-sized chunks from lst."""
     for i in range(0, len(lst), n):
         yield lst[i:i + n]
 
 cbc_key = bytes.fromhex('140b41b22a29beb4061bda66b6747e14')
-#cbc_ct = bytes.fromhex("4ca00ff4c898d61e1edbf1800618fb2828a226d160dad07883d04e008a7897ee2e4b7465d5290d0c0e6c6822236e1daafb94ffe0c5da05d9476be028ad7c1d81")
-cbc_ct = bytes.fromhex("5b68629feb8606f9a6667670b75b38a5b4832d0f26e1ab7da33249de7d4afc48e713ac646ace36e872ad5fb8a512428a6e21364b0c374df45503473c5242a253")
+cbc_ct1 = bytes.fromhex("4ca00ff4c898d61e1edbf1800618fb2828a226d160dad07883d04e008a7897ee2e4b7465d5290d0c0e6c6822236e1daafb94ffe0c5da05d9476be028ad7c1d81")
+cbc_ct2 = bytes.fromhex("5b68629feb8606f9a6667670b75b38a5b4832d0f26e1ab7da33249de7d4afc48e713ac646ace36e872ad5fb8a512428a6e21364b0c374df45503473c5242a253")
 
-cipher = AES.new(cbc_key, AES.MODE_ECB)
+def cbc_decrypt(key, ct):
+    cipher = AES.new(key, AES.MODE_ECB)
 
-out = b""
-for i, c in enumerate(chunks(cbc_ct,16)):
-    if i > 0:
+    out = b""
+    for i, c in enumerate(chunker(ct,16)):
+        if i > 0:
+            out += bytes([_a ^ _b for _a, _b in zip(cipher.decrypt(c), iv)])
+        iv = c
+
+    return out[:-out[-1]]
+
+print(cbc_decrypt(cbc_key, cbc_ct1))
+print(cbc_decrypt(cbc_key, cbc_ct2))
+
+ctr_key = bytes.fromhex("36f18357be4dbd77f050515c73fcf9f2")
+ctr_ct1 = bytes.fromhex("69dda8455c7dd4254bf353b773304eec0ec7702330098ce7f7520d1cbbb20fc388d1b0adb5054dbd7370849dbf0b88d393f252e764f1f5f7ad97ef79d59ce29f5f51eeca32eabedd9afa9329")
+
+iv = ctr_ct1[:8]
+
+def ctr_decrypt(key, iv, ct):
+    cipher = AES.new(key, AES.MODE_ECB)
+
+    out = b""
+    for i, c in enumerate(chunker(ct,16)):
         out += bytes([_a ^ _b for _a, _b in zip(cipher.decrypt(c), iv)])
-    iv = c
+        iv += 1
+    
+    return out[:-out[-1]]
 
-out = out[:-out[-1]]
-
-print(out)
-#set_trace()
+print(ctr_decrypt(ctr_key, iv, ctr_ct1[8:]))
