17cf7936d6692a836fc399a104ae90a278675b68
steve <ssosik@akamai.com>
2021-03-28 21:16:19 -0400

    stash

--- week2/quiz-question-9.py
+++ week2/quiz-question-9.py
@@ -8,14 +8,19 @@ def F(k, x):
         print(t)
     return t
 
-def Fp(k, x):
-    ret = ""
-    t = int(k[0])
+def Fp(_k, _x):
+    if len(_k) != 5:
+        raise Exception("K needs to be 5 bits")
+    if len(_x) != 4:
+        raise Exception("X needs to be 4 bits")
+    k = int(_k,2)
+    x = int(_x,2)
+    t = int(_k[0])
     for i in range(1,5):
-        if x[i-1] == "1":
-            t = t ^ int(k[i])
-        ret += "{}".format(t)
-    return ret
+        if _x[i-1] == "1":
+            print("K {} {} X {} {}".format(_k,k,_x,x))
+            t = t ^ int(_k[i])
+    return format(t, '04b')
 
 def doit(x, e):
     for i in range(0,32):
@@ -25,7 +30,8 @@ def doit(x, e):
         if e == o:
             print(k)
 
+#Fp("01101", "0011")
 #doit("0110", "0011")
-#doit("0101", "1010")
-doit("1110", "0110")
+doit("0101", "1010")
+#doit("1110", "0110")
 set_trace()
