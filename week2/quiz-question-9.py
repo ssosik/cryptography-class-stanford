09d951cc41db528286021f5effcad66aaaf98b58
steve <ssosik@akamai.com>
2021-03-31 10:54:46 -0400

    Checkpoint

--- week2/quiz-question-9.py
+++ week2/quiz-question-9.py
@@ -1,4 +1,5 @@
 from pdb import set_trace
+from itertools import product
 
 def F(k, x):
     t = int(k[0])
@@ -8,27 +9,25 @@ def F(k, x):
         print(t)
     return t
 
-def Fp(_k, _x):
-    if len(_k) != 5:
+def Fp(k, _x):
+    if len(k) != 5:
         raise Exception("K needs to be 5 bits")
     if len(_x) != 4:
         raise Exception("X needs to be 4 bits")
-    k = int(_k,2)
     x = int(_x,2)
-    t = int(_k[0])
+    t = int(k[0], 2)
     for i in range(1,5):
         if _x[i-1] == "1":
-            print("K {} {} X {} {}".format(_k,k,_x,x))
-            t = t ^ int(_k[i])
+            #print("K {} X {} {}".format(k,_x,x))
+            t = t ^ int(k[i],2)
     return format(t, '04b')
 
 def doit(x, e):
-    for i in range(0,32):
-        k = format(i, '{fill}{width}b'.format(width=5, fill=0))
+    for k in product([format(i,'04b') for i in range(0,16)], repeat=5):
         o = Fp(k, x)
-        print("k:{} := o:{}".format(k,o))
+        #print("k:{} := o:{}".format(k,o))
         if e == o:
-            print(k)
+            print("E:{} O:{} + K:{}".format(e, o, k))
 
 #Fp("01101", "0011")
 #doit("0110", "0011")
